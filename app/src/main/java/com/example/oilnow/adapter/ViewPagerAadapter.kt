5dab651066ee2992708231d8fdf1195d83528e64
ka <insideka01@gmail.com>
2023-04-04 17:55:56 +0900

    04.04 home screen에 viewpager2 + auto sliding 구현 but viewpager의 모서리 둥글게가 적용안됨, sliding 속도 느리게 구현해야 함

---
+++ app/src/main/java/com/example/oilnow/adapter/ViewPagerAadapter.kt
@@ -0,0 +1,56 @@
+package com.example.oilnow.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.example.oilnow.databinding.ViewpagerItemLayoutBinding
+
+class ViewPagerAadapter(
+    private val imageList: MutableList<Int>,
+    private val viewPager2: ViewPager2
+) :
+    RecyclerView.Adapter<ViewPagerAadapter.ImageViewHolder>() {
+
+    private lateinit var binding: ViewpagerItemLayoutBinding
+
+    class ImageViewHolder(private var binding: ViewpagerItemLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+            fun bind(item: Int) {
+                binding.apply {
+                    viewpagerImageView.setImageResource(item)
+                }
+            }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewPagerAadapter.ImageViewHolder {
+        binding = ViewpagerItemLayoutBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        return ImageViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerAadapter.ImageViewHolder, position: Int) {
+        val item = imageList[position]
+        holder.bind(item)
+
+        if (position == imageList.size - 1) {
+            viewPager2.post(runnable)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return imageList.size
+    }
+
+    private val runnable = Runnable {
+        imageList.addAll(imageList)
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/example/oilnow/ui/HomeFragment.kt
+++ app/src/main/java/com/example/oilnow/ui/HomeFragment.kt
@@ -1,13 +1,28 @@
 package com.example.oilnow.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.viewpager2.widget.ViewPager2
 import com.example.oilnow.R
+import com.example.oilnow.adapter.ViewPagerAadapter
+import com.example.oilnow.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding
+        get() = _binding!!
+
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var handler: Handler
+    private lateinit var imageList: MutableList<Int>
+    private lateinit var adapter: ViewPagerAadapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +33,59 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewPagerInit()
+
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                handler.removeCallbacks(runnable)
+                handler.postDelayed(runnable, 2500)
+            }
+        })
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        handler.removeCallbacks(runnable)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        handler.postDelayed(runnable, 2500)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun viewPagerInit() {
+        viewPager2 = binding.homeViewpager
+        handler = Handler(Looper.myLooper()!!)
+        imageList = mutableListOf(
+            android.R.color.holo_red_dark,
+            android.R.color.holo_orange_dark,
+            android.R.color.holo_green_light,
+            android.R.color.holo_blue_light,
+            android.R.color.holo_purple
+        )
+
+        adapter = ViewPagerAadapter(imageList, viewPager2)
+        viewPager2.adapter = adapter
+        viewPager2.background = ContextCompat.getDrawable(requireContext(), R.drawable.viewpager2_round_corner)
+    }
+
+    private val runnable = Runnable {
+        viewPager2.currentItem = viewPager2.currentItem + 1
     }
 
 }
